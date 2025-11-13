b9da2ff224232ecb82ec8bcdc08e6603466cc70f
Kirk Bahena <bahenakirk@gmail.com>
2025-11-13 19:53:05 +0800

    Add files via upload

---
+++ Prelim/Activity III/BAHENA_ACTIVITY_lll.java/BAHENA_ACTIVITY_lll.java
@@ -0,0 +1,15 @@
+class BAHENA_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Kirk";
+        char letter = 'K';
+        float height = 1.799f;
+        int age = 18;
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(isOpen);
+    }
+}
